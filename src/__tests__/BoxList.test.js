c15b5b4901d04ecbcdb2ed953866c28c1a919e3c
000xzhou <xzhou92@gmail.com>
2024-05-25 14:29:52 -0400

    testing delete box

--- src/__tests__/BoxList.test.js
+++ src/__tests__/BoxList.test.js
@@ -36,3 +36,35 @@ it("should add new Box", () => {
     backgroundColor: "blue",
   });
 });
+
+it("should delete a Box", () => {
+  const { getByLabelText, getByText, container } = render(<BoxList />);
+  // get inputs
+  const width = getByLabelText("Width:");
+  const height = getByLabelText("Height:");
+  const bgColor = getByLabelText("Background Color:");
+  const addButton = getByText("Submit");
+
+  // add input and submit
+  fireEvent.change(width, { target: { value: "100" } });
+  fireEvent.change(height, { target: { value: "100" } });
+  fireEvent.change(bgColor, { target: { value: "blue" } });
+  fireEvent.click(addButton);
+
+  // get box
+  let newBox = container.querySelector(".box");
+
+  // new box in doc
+  expect(newBox).toBeInTheDocument();
+  expect(newBox).toHaveStyle({
+    width: "100px",
+    height: "100px",
+    backgroundColor: "blue",
+  });
+
+  // get delete btn and press it
+  const deleteButton = getByText("X");
+  fireEvent.click(deleteButton);
+
+  expect(newBox).not.toBeInTheDocument();
+});
