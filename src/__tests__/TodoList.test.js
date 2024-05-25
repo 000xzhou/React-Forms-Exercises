9e9bffc9108919807dfbe1a304001243441b1c95
000xzhou <xzhou92@gmail.com>
2024-05-25 13:30:37 -0400

    add basic test

--- src/__tests__/TodoList.test.js
+++ src/__tests__/TodoList.test.js
@@ -2,11 +2,20 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import TodoList from "../components/Todo/TodoList";
 
+it("should render without crashing", () => {
+  render(<TodoList />);
+});
+
+it("should match snapshot", () => {
+  const { asFragment } = render(<TodoList />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
 it("should add new todo", () => {
   const { getByLabelText, getByText } = render(<TodoList />);
   // get inputs
   const todoInput = getByLabelText("Todo:");
-  const addButton = getByText("Submit");
+  const addButton = getByText("Add");
 
   // add input and submit
   fireEvent.change(todoInput, { target: { value: "100" } });
