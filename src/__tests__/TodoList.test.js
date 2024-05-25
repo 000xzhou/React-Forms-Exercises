e7c609c41326eda901cceccdaf6259992e77cd50
000xzhou <xzhou92@gmail.com>
2024-05-25 14:25:47 -0400

    cancel editing and mark as complete testing

--- src/__tests__/TodoList.test.js
+++ src/__tests__/TodoList.test.js
@@ -75,3 +75,66 @@ it("should edit a todo", () => {
   expect(mango).toBeInTheDocument();
   expect(pizza).not.toBeInTheDocument();
 });
+
+it("should cancel editing a todo", () => {
+  const { queryByText, queryByLabelText, container } = render(<TodoList />);
+  // get inputs
+  const addTodoInput = queryByLabelText("Todo:");
+  const addButton = queryByText("Add");
+  // Add input and submit
+  fireEvent.change(addTodoInput, { target: { value: "Eat some pizza" } });
+  fireEvent.click(addButton);
+
+  let pizza = queryByText("Eat some pizza");
+  expect(pizza).toBeInTheDocument();
+
+  // Get the edit button and click it
+  let editBtn = queryByText("Edit");
+  fireEvent.click(editBtn);
+
+  // Verify the edit form is displayed
+  let editInput = container.querySelector("#editingTodo");
+  expect(editInput).toBeInTheDocument();
+
+  // cancel editing
+  let cancelBtn = queryByText("Cancel");
+  fireEvent.click(cancelBtn);
+
+  // Verify the edit form not display
+  expect(editInput).not.toBeInTheDocument();
+
+  // Verify the original todo is still in the document
+  pizza = queryByText("Eat some pizza");
+  expect(pizza).toBeInTheDocument();
+});
+
+it("should mark a todo complete", () => {
+  const { getByLabelText, queryByText } = render(<TodoList />);
+  // get inputs
+  const todoInput = getByLabelText("Todo:");
+  const addButton = queryByText("Add");
+
+  // add input and submit
+  fireEvent.change(todoInput, { target: { value: "Eat some pizza" } });
+  fireEvent.click(addButton);
+
+  const pizza = queryByText("Eat some pizza");
+  // pizza in doc
+  expect(pizza).toBeInTheDocument();
+
+  const completed = queryByText("Mark as completed");
+  fireEvent.click(completed);
+
+  // Finish eating pizza
+  expect(pizza).toHaveStyle({
+    textDecoration: "line-through",
+  });
+
+  // Mistakes were made! there are more pizza!!!
+  const incomplete = queryByText("Mark as incomplete");
+  fireEvent.click(incomplete);
+
+  expect(pizza).toHaveStyle({
+    textDecoration: "none",
+  });
+});
